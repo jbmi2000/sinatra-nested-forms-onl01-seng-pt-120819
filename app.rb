c2605ee4e6a2ee32a4fa63243c52c2f23f9a4ea9
Jeffrey Marbury <jeff.marbury@gmail.com>
2020-04-05 00:45:40 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -19,7 +19,7 @@ module FormsLab
      binding.pry
      params["pirate"]["ships"].each do |ship_data|
        Ship.new(ship_data["name"], ship_data["type"], ship_data["booty"])
-      # binding.pry
+    
      end
 
      @pirate = Pirate.new(params["pirate"]["name"], params["pirate"]["height"], params["pirate"]["weight"])
